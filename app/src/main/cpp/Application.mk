45905840f177d06b0e9b96b476861039d82feb18
Jomo Fisher <jomof@google.com>
2017-05-05 14:43:15 -0700

    .

--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,3 +1,3 @@
 APP_ABI := all
-NDK_MODULE_PATH += /usr/local/google/home/jomof/projects/ndk-build-meet-cdep/.cdep/modules/ndk-build
+NDK_MODULE_PATH += ../.cdep/modules/ndk-build
 NDK_TOOLCHAIN_VERSION := clang
